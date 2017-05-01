cf87a90709032c2eae85c06f6237c0bc307dc855
Nomaan Ahgharian <get@nomaan.me>
2017-05-01 04:06:11 +0000

    added dance mode + clean up

--- routes/relays.js
+++ routes/relays.js
@@ -1,21 +1,62 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
+const Q = require('q');
 var rpio = require('rpio');
+var sleep = require('sleep');
 
-router.get('/4/on', function(req, res, next) {
+const map = {
+    2: 3,
+    3: 5,
+    4: 7,
+    17: 11
+}
 
-    rpio.open(11, rpio.OUTPUT, rpio.HIGH);
-    rpio.write(11, rpio.LOW);
+const toggle = (pin) => {
+    rpio.open(pin, rpio.OUTPUT)
+    const status = rpio.read(pin);
+    if (status === 1) {
+        rpio.write(pin, rpio.LOW);
+    } else {
+        rpio.write(pin, rpio.HIGH);
+    }
+}
 
-    res.send('hello');
+router.get('/dance', (req, res) => {
+    let dancing = true;
+    const first = req.param('first');
+    const second = req.param('second');
+
+    setTimeout(function() {
+        dancing = false;
+    }, 60000);
+
+    while (dancing) {
+        toggle(second);
+        sleep.msleep(50);
+        toggle(first);
+        sleep.msleep(50);
+        toggle(second);
+        sleep.msleep(50);
+    }
+
+    res.send(first);
 });
 
-router.get('/4/off', function(req, res, next) {
 
-    rpio.open(11, rpio.OUTPUT, rpio.HIGH);
-    rpio.write(11, rpio.HIGH);
+router.get('/3', function(req, res, next) {
+    toggle(7, req);
 
-    res.send('Bye');
+    res.send({ status: rpio.read(7) });
 });
+
+router.get('/4', function(req, res, next) {
+    toggle(11, req);
+
+    res.send({ status: rpio.read(11) });
+});
+
+
 
 module.exports = router;
